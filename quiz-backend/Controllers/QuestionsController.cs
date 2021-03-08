b0349ea08bbd8ee1f4ba4c8cd32effb7cf92ce30
asmaa.ebed <asmaaebed95@gmail.com>
2021-03-08 16:12:13 +0200

    Add project files.

---
+++ quiz-backend/Controllers/QuestionsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace quiz_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Models.Question> Get()
+        {
+            return new Models.Question[] {
+                new Models.Question() { Text = "hello"},
+                new Models.Question() { Text = "asmaa"}
+            };
+        }
+
+        [HttpPost]
+        public void Post([FromBody]Models.Question question)
+        {
+        }
+    }
+}
